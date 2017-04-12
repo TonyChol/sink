82c490e5198a5cd2c4a8be91f321645b26ae5ae5
Zhibin Cai <tonycai1992@gmail.com>
2017-04-12 18:20:29 -0400

    feat(filepath): Added file directory support

--- sink.go
+++ sink.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
+	"io/ioutil"
 	"log"
+
+	"os"
 
 	"github.com/howeyc/fsnotify"
 )
@@ -28,14 +32,55 @@ func watchDir(dir string) {
 	}()
 
 	err = watcher.Watch(dir)
+	defer watcher.RemoveWatch(dir)
+
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
+	files, err := ioutil.ReadDir("./testDir")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		log.Println(file.Name())
+		if file.IsDir() == true {
+			watchDir(file.Name())
+		}
+	}
 	<-done
 }
 
+func getAbsolutePath() (string, error) {
+	return os.Getwd()
+}
+
+func getDirPathFromAgrs() (string, error) {
+	argsArr := os.Args
+	if len(argsArr) < 2 {
+		err := errors.New("You should attach a file directory")
+		return "", err
+	}
+	return os.Args[1], nil
+}
+
 func main() {
+	rootDir, err := getAbsolutePath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	relativeDir, err := getDirPathFromAgrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Args[1]: ", relativeDir)
+
+	log.Print("Root Dir: ", rootDir)
+
 	watchDir("testDir")
 }
