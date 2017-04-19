38a4ec2e083d1e9b2e1e921033a382d09300bf66
Zhibin Cai <tonycai1992@gmail.com>
2017-04-19 15:46:55 -0400

    refactor(base64): Moved base64 functions out

--- fs/fs.go
+++ fs/fs.go
@@ -61,20 +61,12 @@ func AllRecursiveDirsIn(dirPath string) []string {
 	return dirSlice
 }
 
-// Base64StrFromFile : Encode the file into the base64 string
-func Base64StrFromFile(path string) (string, error) {
-	buff, err := ioutil.ReadFile(path)
-	if err != nil {
-		return "", err
 	}
 
 	return base64.StdEncoding.EncodeToString(buff), nil
 
 }
 
-// FileFromBase64Str : Decode the base64 string into a file
-func FileFromBase64Str(code string, dest string) error {
-	buff, err := base64.StdEncoding.DecodeString(code)
 
 	if err != nil {
 		return err
